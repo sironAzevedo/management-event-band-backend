7dd763332d2845aa3e383b048335c2a79a7e37e3
siron azevedo <sirondba@gmail.com>
2021-03-26 12:16:07 -0300

    Implementando java

---
+++ src/main/java/com/geb/ManagementEventBandBackendApplication.java
@@ -0,0 +1,13 @@
+package com.geb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementEventBandBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementEventBandBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geb/ManagementEventBandBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementEventBandBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
